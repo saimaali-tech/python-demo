33cf30b393361525cc4f8771a1ba9c18626c614b
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-25 15:32:13 +0500

    subtracted numbers

--- file.py
+++ file.py
@@ -4,5 +4,5 @@ num2 = float(input("Enter the  your second number: "))
 num3 = float(input("Enter the your  third number: "))
 num4 = float(input("Enter the  your fourth number: "))
 
-sum_result = num1 + num2 + num3 + num4
-print(f"The sum of the four numbers is: {sum_result}")
+sub_result = num1 - num2 - num3 - num4
+print(f"The sum of the four numbers is: {sub_result}")
