d16d0d64dd36d5296677a223bf44094e0d0a9521
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-25 15:57:49 +0500

    commented some content in file.py

--- file.py
+++ file.py
@@ -4,5 +4,31 @@ num2 = int(input("Enter the  your second number: "))
 num3 = int(input("Enter the your  third number: "))
 num4 = int(input("Enter the  your fourth number: "))
 
-mul_result = num1 * num2 * num3 * num4
-print(f"The sum of the four numbers is: {mul_result}")
+add_result = num1 + num2 + num3 + num4
+print(f"The sum of the four numbers is: {add_result}")
+
+
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
+
+# sub_result = num1 - num2 - num3 - num4
+# print(f"The sum of the four numbers is: {sub_result}")
+
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
+
+# div_result = num1 /num2 / num3 / num4
+# print(f"The sum of the four numbers is: {div_result}")
+
+
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
+
+# mul_result = num1 * num2 * num3 * num4
+# print(f"The sum of the four numbers is: {mul_result}")
