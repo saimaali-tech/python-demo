eb9ae813cf7a4792ebe4160f1db541906e9fcc79
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-25 15:23:15 +0500

    change the content in file.py

--- file.py
+++ file.py
@@ -1,8 +1,8 @@
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
-num4 = float(input("Enter the fourth number: "))
+num1 = float(input("Enter the your first number: "))
+num2 = float(input("Enter the  your second number: "))
+num3 = float(input("Enter the your  third number: "))
+num4 = float(input("Enter the  your fourth number: "))
 
 sum_result = num1 + num2 + num3 + num4
 print(f"The sum of the four numbers is: {sum_result}")
