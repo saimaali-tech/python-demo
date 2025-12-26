d31dccf7a4ffffdbd48c155b88ecbf34b3067402
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-26 21:38:09 +0500

    result for two students only

--- file.py
+++ file.py
@@ -8,27 +8,27 @@ add_result = num1 + num2 + num3 + num4
 print(f"The sum of the four numbers is: {add_result}")
 
 
-num1 = int(input("Enter the your first number: "))
-num2 = int(input("Enter the  your second number: "))
-num3 = int(input("Enter the your  third number: "))
-num4 = int(input("Enter the  your fourth number: "))
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
 
-sub_result = num1 - num2 - num3 - num4
-print(f"The sum of the four numbers is: {sub_result}")
+# sub_result = num1 - num2 - num3 - num4
+# print(f"The sum of the four numbers is: {sub_result}")
 
-num1 = int(input("Enter the your first number: "))
-num2 = int(input("Enter the  your second number: "))
-num3 = int(input("Enter the your  third number: "))
-num4 = int(input("Enter the  your fourth number: "))
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
 
-div_result = num1 /num2 / num3 / num4
-print(f"The sum of the four numbers is: {div_result}")
+# div_result = num1 /num2 / num3 / num4
+# print(f"The sum of the four numbers is: {div_result}")
 
 
-num1 = int(input("Enter the your first number: "))
-num2 = int(input("Enter the  your second number: "))
-num3 = int(input("Enter the your  third number: "))
-num4 = int(input("Enter the  your fourth number: "))
+# num1 = int(input("Enter the your first number: "))
+# num2 = int(input("Enter the  your second number: "))
+# num3 = int(input("Enter the your  third number: "))
+# num4 = int(input("Enter the  your fourth number: "))
 
-mul_result = num1 * num2 * num3 * num4
-print(f"The sum of the four numbers is: {mul_result}")
+# mul_result = num1 * num2 * num3 * num4
+# print(f"The sum of the four numbers is: {mul_result}")
