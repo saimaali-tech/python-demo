cb4fac52398ca0328a6a0de72b513ed09163adb3
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-28 09:46:05 +0500

    created multiple buttons

--- button.py
+++ button.py
@@ -1,17 +1,27 @@
 import tkinter as tk
 
-def on_button_click():
-    print("Button was clicked!")
+def on_button1_click():
+    print("Button 1 was clicked!")
+
+def on_button2_click():
+    print("Button 2 was clicked!")
+
+def on_button3_click():
+    print("Button 3 was clicked!")
 
 # Create the main window
 root = tk.Tk()
-root.title("Simple Button Example")
+root.title("Multiple Buttons Example")
 
-# Create a button widget
-button = tk.Button(root, text="Click Me", command=on_button_click)
+# Create button widgets
+button1 = tk.Button(root, text="Button 1", command=on_button1_click)
+button2 = tk.Button(root, text="Button 2", command=on_button2_click)
+button3 = tk.Button(root, text="Button 3", command=on_button3_click)
 
-# Pack the button into the window
-button.pack(pady=20)
+# Pack the buttons into the window
+button1.pack(pady=10)
+button2.pack(pady=10)
+button3.pack(pady=10)
 
 # Run the Tkinter event loop
 root.mainloop()
