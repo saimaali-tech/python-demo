f4905cf6505a2ed7c16b52ec3419ac9352eb4277
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-06 14:45:14 +0500

    change the numbers

--- datatypes.py
+++ datatypes.py
@@ -1,18 +1,18 @@
-# # Data Type in Python
-# # A data type tells Python:
-# # what kind of data a variable holds
-# # what operations can be performed on it
+# Data Type in Python
+# A data type tells Python:
+# what kind of data a variable holds
+# what operations can be performed on it
 
-# # Numeric Data Types
-# a = 10
-# b = -25
-# print(type(b))   # <class 'int'>
+# Numeric Data Types
+a = 10
+b = -25
+print(type(b))   # <class 'int'>
 
 
-# # float
-# x = 10.5
-# y = -3.14
-# print(type(x))   # <class 'float'>
+# float
+x = 10.5
+y = -3.14
+print(type(x))   # <class 'float'>
 
 # # complex
 # z = 3 + 4j
