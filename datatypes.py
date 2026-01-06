fb2226eb5b441a778525920af1aae38a576e49bc
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-06 14:28:27 +0500

    datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+# Data Type in Python
+# A data type tells Python:
+# what kind of data a variable holds
+# what operations can be performed on it
+
+# Numeric Data Types
+a = 10
+b = -25
+print(type(b))   # <class 'int'>
