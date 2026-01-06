de8d485244f687918a83e84f7353d29f4a33fffd
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-06 14:33:30 +0500

    string datatype in python

--- datatypes.py
+++ datatypes.py
@@ -1,19 +1,24 @@
-# Data Type in Python
-# A data type tells Python:
-# what kind of data a variable holds
-# what operations can be performed on it
+# # Data Type in Python
+# # A data type tells Python:
+# # what kind of data a variable holds
+# # what operations can be performed on it
 
-# Numeric Data Types
-a = 10
-b = -25
-print(type(b))   # <class 'int'>
+# # Numeric Data Types
+# a = 10
+# b = -25
+# print(type(b))   # <class 'int'>
 
 
-# float
-x = 10.5
-y = -3.14
-print(type(x))   # <class 'float'>
+# # float
+# x = 10.5
+# y = -3.14
+# print(type(x))   # <class 'float'>
 
-# complex
-z = 3 + 4j
-print(type(z))   # <class 'complex'>
+# # complex
+# z = 3 + 4j
+# print(type(z))   # <class 'complex'>
+
+# str (String)
+name = "Saima"
+msg = 'Hello World'
+print(name,msg)
