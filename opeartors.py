72f59cac45fa83a67e417e48832a19922d19453a
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-07 21:53:00 +0500

    operators predence in python

--- opeartors.py
+++ opeartors.py
@@ -1,71 +1,75 @@
-# Arithmetic Operators
-a = 10
-b = 3
+# # Arithmetic Operators
+# a = 10
+# b = 3
 
-print(a + b)    # 13
-print(a - b)    # 7
-print(a * b)    # 30
-print(a / b)    # 3.3333
-print(a % b)    # 1
-print(a ** b)   # 1000
-print(a // b)   # 3
-
-
-# Assignment Operators
-x = 10
-x += 5
-print(x)   # 15
-
-x *= 2
-print(x)   # 30
+# print(a + b)    # 13
+# print(a - b)    # 7
+# print(a * b)    # 30
+# print(a / b)    # 3.3333
+# print(a % b)    # 1
+# print(a ** b)   # 1000
+# print(a // b)   # 3
 
 
-# Comparison (Relational) Operators
+# # Assignment Operators
+# x = 10
+# x += 5
+# print(x)   # 15
 
-a = 10
-b = 20
-
-print(a == b)   # False
-print(a != b)   # True
-print(a > b)    # False
-print(a < b)    # True
+# x *= 2
+# print(x)   # 30
 
 
-# Logical Operators
-x = 5
+# # Comparison (Relational) Operators
 
-print(x > 3 and x < 10)   # True
-print(x > 10 or x < 8)    # True
-print(not(x > 3))         # False
+# a = 10
+# b = 20
 
-# Bitwise Operators
-a = 5   # 0101
-b = 3   # 0011
-
-print(a & b)   # 1
-print(a | b)   # 7
-print(a ^ b)   # 6
-print(~a)      # -6
-print(a << 1)  # 10
-print(a >> 1)  # 2
-
-# //Membership Operators
-numbers = [1, 2, 3, 4, 5]
-
-print(3 in numbers)        # True
-print(10 not in numbers)  # True
+# print(a == b)   # False
+# print(a != b)   # True
+# print(a > b)    # False
+# print(a < b)    # True
 
 
-# Identity Operators
-a = [1, 2, 3]
-b = a
-c = [1, 2, 3]
+# # Logical Operators
+# x = 5
 
-print(a is b)      # True
-print(a is c)      # False
-print(a == c)      # True
+# print(x > 3 and x < 10)   # True
+# print(x > 10 or x < 8)    # True
+# print(not(x > 3))         # False
 
-# Ternary (Conditional) Operator
-age = 18
-status = "Adult" if age >= 18 else "Minor"
-print(status)
+# # Bitwise Operators
+# a = 5   # 0101
+# b = 3   # 0011
+
+# print(a & b)   # 1
+# print(a | b)   # 7
+# print(a ^ b)   # 6
+# print(~a)      # -6
+# print(a << 1)  # 10
+# print(a >> 1)  # 2
+
+# # //Membership Operators
+# numbers = [1, 2, 3, 4, 5]
+
+# print(3 in numbers)        # True
+# print(10 not in numbers)  # True
+
+
+# # Identity Operators
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+
+# print(a is b)      # True
+# print(a is c)      # False
+# print(a == c)      # True
+
+# # Ternary (Conditional) Operator
+# age = 18
+# status = "Adult" if age >= 18 else "Minor"
+# print(status)
+
+# Operator Precedence
+result = 10 + 5 * 2
+print(result)   # 20 (multiplication first)
