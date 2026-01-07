b0dee7820e7d1510ba884b10ec5eeac43fab5046
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-07 21:49:11 +0500

    bitwise operators in python

--- opeartors.py
+++ opeartors.py
@@ -38,3 +38,13 @@ print(x > 3 and x < 10)   # True
 print(x > 10 or x < 8)    # True
 print(not(x > 3))         # False
 
+# Bitwise Operators
+a = 5   # 0101
+b = 3   # 0011
+
+print(a & b)   # 1
+print(a | b)   # 7
+print(a ^ b)   # 6
+print(~a)      # -6
+print(a << 1)  # 10
+print(a >> 1)  # 2
