1fdeaabb27e10d79564016e9e621261f45878a54
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-07 21:41:56 +0500

    arithmetic opeartors in python

---
+++ opeartors.py
@@ -0,0 +1,11 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print(a + b)    # 13
+print(a - b)    # 7
+print(a * b)    # 30
+print(a / b)    # 3.3333
+print(a % b)    # 1
+print(a ** b)   # 1000
+print(a // b)   # 3
