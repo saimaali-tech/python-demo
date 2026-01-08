8f79c06bac51f39c09d0442d905c683e73e5a3f8
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-08 15:27:11 +0500

    if-else-else  statements in python

--- conditional.py
+++ conditional.py
@@ -11,3 +11,15 @@ if age >= 18:
     print("You can vote")
 else:
     print("You cannot vote")
+
+# if - elif - else (Multiple Conditions)
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
