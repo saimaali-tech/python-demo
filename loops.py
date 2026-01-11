ccaf3c26877cc3763a926e2896f9aab704ae386c
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-11 21:01:38 +0500

    while loo

--- loops.py
+++ loops.py
@@ -1,23 +1,30 @@
 # Loop through a list
-fruits = ["apple", "banana", "mango"]
+# fruits = ["apple", "banana", "mango"]
 
-for fruit in fruits:
-    print(fruit)
-
-
-# Loop through a string
-name = "Python"
-
-for char in name:
-    print(char)
+# for fruit in fruits:
+#     print(fruit)
 
 
-# Loop with range()
-for i in range(5):
+# # Loop through a string
+# name = "Python"
+
+# for char in name:
+#     print(char)
+
+
+# # Loop with range()
+# for i in range(5):
+#     print(i)
+
+
+# # range(stop)
+# range(5)   # 0 to 4
+# range(2, 6)  # 2 to 5
+# range(1, 10, 2)  # 1, 3, 5, 7, 9
+
+# while Loop in Python
+i = 1
+
+while i <= 5:
     print(i)
-
-
-# range(stop)
-range(5)   # 0 to 4
-range(2, 6)  # 2 to 5
-range(1, 10, 2)  # 1, 3, 5, 7, 9
+    i += 1
