79a59076a524ec6cd43643957131cce8f107ed45
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-13 16:17:14 +0500

    commentin the content

--- loops.py
+++ loops.py
@@ -51,3 +51,9 @@ for i in range(5):
 for i in range(1, 4):
     for j in range(1, 4):
         print(i, j)
+
+# Loop with else (Less Known but Important)
+for i in range(3):
+    print(i)
+else:
+    print("Loop completed successfully")
