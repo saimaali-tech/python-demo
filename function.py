738181920c6ac825ca41e5fb67070e36e06bbc69
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-16 22:43:41 +0500

    comment out the content

--- function.py
+++ function.py
@@ -1,15 +1,15 @@
 # What is a Function
 # A function is a reusable block of code that performs a specific task.
 
-def add():
-    a = 5
-    b = 3
-    print(a + b)
+# def add():
+#     a = 5
+#     b = 3
+#     print(a + b)
 
-add()
+# add()
 
-def greet(name):
-    print("Hello", name)
+# def greet(name):
+#     print("Hello", name)
 
-greet("Saima")
+# greet("Saima")
 
