bc277141cb7640c0e6f53e24380e0e03e1180588
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-17 21:36:44 +0500

    changed the color

--- button.py
+++ button.py
@@ -19,11 +19,11 @@ custom_font = tkfont.Font(family="Arial", size=15, weight="bold")
 
 # Create button widgets with larger size, custom font, and colors
 button1 = tk.Button(root, text="Button 1", command=on_button1_click, width=25, height=3,
-                    font=custom_font, bg="lightblue", fg="white")
+                    font=custom_font, bg="red", fg="white")
 button2 = tk.Button(root, text="Button 2", command=on_button2_click, width=25, height=3,
-                    font=custom_font, bg="green", fg="white")
+                    font=custom_font, bg="blue", fg="white")
 button3 = tk.Button(root, text="Button 3", command=on_button3_click, width=25, height=3,
-                    font=custom_font, bg="gray", fg="white")
+                    font=custom_font, bg="green", fg="white")
 
 # Pack the buttons into the window with some padding
 button1.pack(pady=10)
