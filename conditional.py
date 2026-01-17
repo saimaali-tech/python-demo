0eb78b24a673185485c83d8d04435b31f75ac5b4
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-17 21:40:37 +0500

    changed the colors

--- conditional.py
+++ conditional.py
@@ -87,7 +87,7 @@
 username = "admin"
 password = "4567"
 
-if username == "admin" and password == "1234":
+if username == "admin" and password == "567889":
     print("Login successful")
 else:
     print("Invalid credentials")
